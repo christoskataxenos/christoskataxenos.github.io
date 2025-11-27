25c52bdd44097c2b0bad39a296a3ff7f1db72f38
Christos Kataxenos <christosk8926@gmail.com>
2025-11-27 17:17:52 +0100

    Add output configuration to next.config.mjs
    Added output configuration to prevent README display issue.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // <--- ΑΥΤΟ ΕΛΕΙΠΕ! Χωρίς αυτό βλέπεις μόνο το Readme.
+  output: 'export',  // 
   images: {
     unoptimized: true, // Απαραίτητο για να φαίνονται οι εικόνες στο GitHub
   },
