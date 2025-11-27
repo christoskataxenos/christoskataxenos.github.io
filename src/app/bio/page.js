c60a818be6a2519b0c3ed04774180224fb3a03fd
Christos Kataxenos <christosk8926@gmail.com>
2025-11-27 17:54:15 +0100

    Remove metadata export from BioPage

--- src/app/bio/page.js
+++ src/app/bio/page.js
@@ -3,9 +3,7 @@ import UnderConstruction from '../../components/UnderConstruction';
 import { UserIcon } from '../../components/Icons';
 import SpotlightCard from '../../components/SpotlightCard';
 
-export const metadata = {
-  title: "Biography | Christos Kataxenos",
-};
+
 
 export default function BioPage() {
   return <UnderConstruction title="Biography" Icon={UserIcon} SpotlightCard={SpotlightCard} />;
