086ca4039cd7428df19cac5dafcf6e99c7a15390
Christos Kataxenos <christosk8926@gmail.com>
2025-11-27 17:54:48 +0100

    Remove metadata export from PortfolioPage
    Removed metadata export for the PortfolioPage.

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -3,9 +3,7 @@ import UnderConstruction from '../../components/UnderConstruction';
 import { CameraIcon } from '../../components/Icons';
 import SpotlightCard from '../../components/SpotlightCard';
 
-export const metadata = {
-  title: "Photography | Christos Kataxenos",
-};
+
 
 export default function PortfolioPage() {
   return <UnderConstruction title="Photography" Icon={CameraIcon} SpotlightCard={SpotlightCard} />;
